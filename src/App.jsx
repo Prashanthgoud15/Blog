e35fe15875a848470672efbf0d75a55cd58f039f
Prashanth Goud <goudprashanth691@gmail.com>
2026-02-24 21:18:34 +0530

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//import './App.css'
+//import Login from './Pages/Login/Login'
+import Registration from './Pages/Registration/Registration'
+function App() {
+
+
+  return (
+    <>
+
+     {/* <Login /> */}
+     <Registration />
+    </>
+  )
+}
+
+export default App
